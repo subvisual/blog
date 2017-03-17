996927b54e575be18559a070d281d031b89d2716
Miguel Palhas <mpalhas@gmail.com>
2017-03-17 23:03:12 +0000

    Importing legacy Stylesheets

--- vendor/blue/javascripts/blue.js
+++
@@ -1,6 +0,0 @@
-//= require ./blue/_scroll
-//= require ./blue/_scroll_state
-//= require ./blue/_nav_logo
-//= require ./blue/_nav_visibility
-//= require ./blue/_nav
-//= require ./blue/_nav_state
