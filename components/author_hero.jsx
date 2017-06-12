63eccdf5b03791b06560b1990efaaf76343aac99
Miguel Palhas <mpalhas@gmail.com>
2017-06-12 13:56:16 +0100

    Adds social links to Author hero

--- components/author_hero.jsx
+++ components/author_hero.jsx
@@ -5,6 +5,7 @@ import { prefixLink } from 'gatsby-helpers';
 
 import Hero from './hero';
 import Picture from './author/picture';
+import SocialLinks from './social_links';
 import Authors from '../data/authors.yaml';
 
 import GoBackIcon from '../images/icon-go-back.svg';
@@ -17,10 +18,15 @@ const AuthorHero = ({ author }) => {
     <div className="u-navPlaceholder" />
     <div className="u-defaultThenLargeMargin" />
     <div className="PostWidthConstrainer">
-      <h1 className="HeroHeading HeroHeading--withPicture">
+      <h1 className="HeroHeading HeroHeading--withPicture HeroHeading--withLinks">
         <Picture className="HeroHeading-picture" slug={author} />
         {authorData.name}
+        <div className="HeroHeading-links">
+          <SocialLinks {...authorData} />
+        </div>
       </h1>
+      <div className="u-xSmallMargin" />
+      <p className="HeroText">{authorData.bio}</p>
       <div className="u-smallPadding" />
       <a href={prefixLink('/')} className="BackToIndex">
         <div className="BackToIndex-icon" dangerouslySetInnerHTML={{ __html: GoBackIcon }} />
---
+++ components/social_links.jsx
@@ -0,0 +1,59 @@
+import React, { PropTypes } from 'react';
+
+import twitterIcon from '../images/icons/twitter.svg';
+import githubIcon from '../images/icons/github.svg';
+import dribbbleIcon from '../images/icons/dribbble.svg';
+
+const Icons = {
+  twitter: twitterIcon,
+  github: githubIcon,
+  dribbble: dribbbleIcon,
+};
+
+export default class SocialLinks extends React.Component {
+  static propTypes = {
+    /* eslint-disable react/no-unused-prop-types */
+    twitter: PropTypes.string,
+    github: PropTypes.string,
+    dribbble: PropTypes.string,
+    /* eslint-enable react/no-unused-prop-types */
+  }
+
+  static defaultProps = {
+    twitter: null,
+    github: null,
+    dribbble: null,
+  }
+
+  renderLink(name) {
+    if (!this.props[name]) {
+      return null;
+    }
+
+    /* eslint-disable react/no-danger */
+    return <a
+      key={name}
+      className={`SocialLinks SocialLinks-${name}`}
+      href={`https://${name}.com/${this.props[name]}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="SocialLinks-icon" dangerouslySetInnerHTML={{ __html: Icons[name] }} />
+    </a>;
+    /* eslint-enable react/no-danger */
+  }
+
+  renderLinks() {
+    return [
+      this.renderLink('twitter'),
+      this.renderLink('github'),
+      this.renderLink('dribbble'),
+    ];
+  }
+
+  render() {
+    return <div className="SocialLinks">
+      {this.renderLinks()}
+    </div>;
+  }
+}
